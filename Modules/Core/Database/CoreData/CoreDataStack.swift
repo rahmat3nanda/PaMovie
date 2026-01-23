d8d7626f05a6389985dc4b1f977084c4a668846e
Rahmat Trinanda <rahmat3nanda@gmail.com>
2026-01-23 11:11:56 +0700

    Add CoreDataStack to Database module and remove Database.swift

---
+++ Modules/Core/Database/CoreData/CoreDataStack.swift
@@ -0,0 +1,42 @@
+//
+//  CoreDataStack.swift
+//  Database
+//
+//  Created by Rahmat Trinanda Pramudya Amar on 23/01/26.
+//
+
+import CoreData
+
+public final class CoreDataStack {
+    
+    public static let shared = CoreDataStack()
+    
+    public let container: NSPersistentContainer
+    
+    private init(inMemory: Bool = false) {
+        container = NSPersistentContainer(name: "PaMovie")
+        
+        if inMemory {
+            container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "/dev/null")
+        }
+        
+        container.loadPersistentStores { _, error in
+            if let error {
+                fatalError("CoreData failed: \(error)")
+            }
+        }
+        
+        container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        container.viewContext.automaticallyMergesChangesFromParent = true
+    }
+    
+    public var context: NSManagedObjectContext {
+        container.viewContext
+    }
+    
+    public func save() throws {
+        if context.hasChanges {
+            try context.save()
+        }
+    }
+}
--- Modules/Core/Database/Database.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Database.swift
-//  Database
-//
-//  Created by Rahmat Trinanda Pramudya Amar on 23/01/26.
-//
-
-import Foundation
-

20f868adc0aa3d20f0bdbb3dc5dc56b73458c92a
Rahmat Trinanda <rahmat3nanda@gmail.com>
2026-01-23 11:18:04 +0700

    Add Database framework to Core module and MoviesList framework to Features module

---
+++ Modules/Features/MoviesListTests/MoviesListTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesListTests.swift
+//  MoviesListTests
+//
+//  Created by Rahmat Trinanda Pramudya Amar on 23/01/26.
+//
+
+import Testing
+@testable import MoviesList
+
+struct MoviesListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

ead2679bccd4c8c385ad6ff5f6020d86736b6b86
Rahmat Trinanda <rahmat3nanda@gmail.com>
2026-01-23 11:06:31 +0700

    Add Database framework and tests to Core module

---
+++ Modules/Core/Database/Database.swift
@@ -0,0 +1,9 @@
+//
+//  Database.swift
+//  Database
+//
+//  Created by Rahmat Trinanda Pramudya Amar on 23/01/26.
+//
+
+import Foundation
+
---
+++ Modules/Core/DatabaseTests/DatabaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  DatabaseTests.swift
+//  DatabaseTests
+//
+//  Created by Rahmat Trinanda Pramudya Amar on 23/01/26.
+//
+
+import Testing
+@testable import Database
+
+struct DatabaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
